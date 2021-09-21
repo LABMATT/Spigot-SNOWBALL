809c55cd5651b618b0fc44b24f86c9767d55f819
LABMATT <matt42764@gmail.com>
2021-09-21 23:25:10 +1000

    Creation of the snowball project

---
+++ src/main/java/labmatt/space/Snowball.java
@@ -0,0 +1,17 @@
+package labmatt.space;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Snowball extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
